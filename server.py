24fdc174a6957c9aa7e337838377b7ed489bf2c2
FPereiraI <fabian.pereira.i@ug.uchile.cl>
2026-08-21 00:49:46 -0400

    feat: server now reads json config file

--- server.py
+++ server.py
@@ -4,18 +4,37 @@ import json
 
 HOST = "127.0.0.1"
 
+with open(sys.argv[1]) as file:
+    data = json.load(file)
+    user = data["user"]
+    print(user)
+
+body = """<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>TEST</title>
+</head>
+<body>
+    <h1>Esto es una prueba</h1>
+    <h3>Probando</h3>
+</body>
+"""
+body_bytes = body.encode("utf-8")
+content_length = len(body_bytes)
+
+header = f"HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=utf-8\r\nX-ElQuePregunta: Fabián\r\nContent-Length: {content_length}\r\nConnection: close\r\n\r\n"
+
+msg = header+body
+
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_socket.bind((HOST, 8000))
 server_socket.listen(1)
 
-
 new_socket, new_socket_address = server_socket.accept()
 r_bytes = new_socket.recv(1024)
 
-print(r_bytes)
+new_socket.send(msg.encode("utf-8"))
 
 new_socket.close()
 server_socket.close()
-
-
-
